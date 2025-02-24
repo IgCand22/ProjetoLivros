89851542b8a51d2efcff9307f042053f6e73edff
IgCand22 <icand32@gmail.com>
2025-02-24 20:17:18 -0300

    Aula de update e delet dados do banco e encerramento do projeto

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ app.post('/books/insertbook', (req, res) => {
     const title = req.body.title
     const pageqty = req.body.pageqty
 
-    const sql = `INSERT INTO books (title, page) VALUES ('${title}', '${pageqty}')`
+    const sql = `INSERT INTO books (??, ??) VALUES (?, ?)`
+    const data = ['title', 'page', title, pageqty]
 
-    conn.query(sql, function(err) {
+    conn.query(sql, data, function(err) {
         if(err) {
             console.log(err)
         }
@@ -59,9 +60,10 @@ app.get('/books', (req, res) => {
 app.get('/books/:id', (req, res) => {
     const id = req.params.id
 
-    const sql = `SELECT * FROM books WHERE id = ${id}`
+    const sql = `SELECT * FROM books WHERE ?? = ?`
+    const data = ['id', id]
 
-    conn.query(sql, function(err, data) {
+    conn.query(sql, data, function(err, data) {
         if(err) {
             console.log(err)
             return
@@ -70,6 +72,58 @@ app.get('/books/:id', (req, res) => {
         const book = data[0]
 
         res.render('book', { book })
+    })
+})
+
+app.get('/books/edit/:id', (req, res) => {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE ?? = ?`
+    const data = ['id', id]
+
+    conn.query(sql, data, function(err, data) {
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+        res.render('editbook', { book })
+    })
+})
+
+app.post('/books/updatebook', (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `UPDATE books SET ?? = ?, ?? = ? WHERE ?? = ?`
+    const data = ['title', title, 'page', pageqty, 'id', id]
+
+    conn.query(sql, data, function(err) {
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.post('/books/remove/:id', (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `DELETE FROM books WHERE ?? = ?`
+    const data = ['id', id]
+
+    conn.query(sql, data, function(err) {
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
     })
 })
 
